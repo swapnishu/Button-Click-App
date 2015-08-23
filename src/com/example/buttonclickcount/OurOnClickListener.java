19f47a9ceb33902a3bbd3492f3e393a286ec1148
Swapnil Saurav <swapnishu@rediffmail.com>
2015-08-23 20:08:20 +0530

    Initial commit

---
+++ src/com/example/buttonclickcount/OurOnClickListener.java
@@ -0,0 +1,18 @@
+package com.example.buttonclickcount;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class OurOnClickListener implements OnClickListener {
+
+	public OurOnClickListener(MainActivity mainActivity) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
