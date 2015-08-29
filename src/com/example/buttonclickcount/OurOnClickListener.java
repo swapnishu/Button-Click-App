79630c81e1d0356eb85aaebe80d0eb6087b6604d
Swapnil Saurav <swapnishu@rediffmail.com>
2015-08-29 23:44:26 +0530

    basic app made

--- src/com/example/buttonclickcount/OurOnClickListener.java
+++ src/com/example/buttonclickcount/OurOnClickListener.java
@@ -1,18 +1,20 @@
 package com.example.buttonclickcount;
 
+import android.app.Activity;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 public class OurOnClickListener implements OnClickListener {
-
+	
+	MainActivity caller;
 	public OurOnClickListener(MainActivity mainActivity) {
 		// TODO Auto-generated constructor stub
+		this.caller = mainActivity;
 	}
 
-	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-
+		caller.theTextView.setText("The button got clicked");
 	}
 
 }
