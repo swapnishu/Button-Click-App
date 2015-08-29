66a71fbf239a2c4320a47a4fcd96d6031bc1e800
Swapnil Saurav <swapnishu@rediffmail.com>
2015-08-29 23:55:56 +0530

    Counter logic added

--- src/com/example/buttonclickcount/OurOnClickListener.java
+++ src/com/example/buttonclickcount/OurOnClickListener.java
@@ -7,14 +7,20 @@ import android.view.View.OnClickListener;
 public class OurOnClickListener implements OnClickListener {
 	
 	MainActivity caller;
+	private int count;
 	public OurOnClickListener(MainActivity mainActivity) {
 		// TODO Auto-generated constructor stub
 		this.caller = mainActivity;
+		this.count = 0;
 	}
 
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		caller.theTextView.setText("The button got clicked");
+		count = count + 1;
+		String outputString = "The button itself has been clicked " + count + " time";
+		if(count != 1)
+			outputString += "s";
+		caller.theTextView.setText(outputString);
 	}
 
 }
